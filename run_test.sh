c335afb5bc5a0a3eab0a100ff496f3d1860a0484
유지훈 <jeehoon.yoo@ridi.com>
2017-08-25 18:32:15 +0900

    Reconstruct project structure; put json-server-related files to ./json-server.

---
+++ run_test.sh
@@ -0,0 +1,24 @@
+docker-compose -f ./json-server/docker-compose.test.yml down
+docker-compose -f ./json-server/docker-compose.test.yml up --build -d
+
+echo "Waiting until container becomes healthy..."
+
+retries=0
+while ! docker ps | grep json-server | grep healthy; do
+    sleep 1
+
+    if [ "$retries" -gt 10 ]
+    then
+        break
+    fi
+
+    retries=$(($retries+1))
+done
+
+if [ "$retries" -gt 10 ]
+then
+    echo "Fail to load json-server."
+else
+    echo "json-server is ready."
+    tox
+fi
